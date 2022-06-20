6312aaab99943850f4d6a57a117dedcf38254f02
RachaelKivuti <kivutingatha@gmail.com>
2022-06-20 20:24:48 +0000

    checker error fix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,6 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return True
-    except (ValueError, TypeError):
-        return False
+        ret = True
+    except Exception:
+        ret = False
+    return (ret)
