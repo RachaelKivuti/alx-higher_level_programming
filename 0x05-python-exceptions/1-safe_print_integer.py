54fdeda75042c0689104e33805217fe9f072365c
RachaelKivuti <kivutingatha@gmail.com>
2022-06-20 20:07:40 +0000

    error fix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    try:
-        print("{:d}".format(value))
-        ret = True
-    except:
-        ret = False
-    return (ret)
+	try:
+	print("{:d}".format(value))
+	ret = True
+	except:
+	ret = False
+	return (ret)
