78fd969c0f3c5972cc4ec0d0c14685bc4e807f8a
RachaelKivuti <kivutingatha@gmail.com>
2022-09-06 19:49:32 +0000

    fix

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,8 @@
-
 #!/usr/bin/node
-const origin = require('./100-data').list;
-const newl = origin.map((e, idx) => e * idx);
-console.log(origin);
-console.log(newl);
+const list = require('./100-data').list;
+const newList = list.map(function (num, index) {
+  return num * index;
+});
+
+console.log(list);
+console.log(newList);
